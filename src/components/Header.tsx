b14f22096680ac66a2cf38a8aad6aaaf41837e8a
munaciella <franes88@gmail.com>
2025-01-21 14:23:16 +0000

    add shadcn ui & add a header

---
+++ src/components/Header.tsx
@@ -0,0 +1,56 @@
+import { Briefcase, HomeIcon, MessageSquare, MessagesSquareIcon, SearchIcon, UsersIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+  return (
+    <div className="flex items-center p-2 max-w-6xl mx-auto">
+      <Image
+        className="rounded-lg"
+        src="https://links.papareact.com/b3z"
+        alt="Networking Platform"
+        width={40}
+        height={40}
+      />
+
+      <div className="flex-1">
+        <form className="flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96">
+            <SearchIcon className='h-4 text-gray-600'/>
+            <input
+                type="text"
+                placeholder="Search"
+                className="bg-transparent flex-1 outline-none"
+            />
+        </form>
+      </div>
+
+      <div className='flex items-center space-x-4 px-6'>
+        <Link href="/" className='icon'>
+        <HomeIcon className='h-5'/>
+        <p>Home</p>
+        </Link>
+
+        <Link href="" className='icon hidden md:flex'>
+        <UsersIcon className='h-5'/>
+        <p>Network</p>
+        </Link>
+
+        <Link href="" className='icon hidden md:flex'>
+        <Briefcase className='h-5'/>
+        <p>Jobs</p>
+        </Link>
+
+        <Link href="" className='icon'>
+        <MessagesSquareIcon className='h-5'/>
+        <p>Messaging</p>
+        </Link>
+
+        {/* User Button */}
+
+        {/* Sign In Button */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
