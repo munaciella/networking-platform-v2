77fcb5c985ea42aeae3a82e7e3bb41eb414f6ed3
munaciella <franes88@gmail.com>
2026-04-25 02:19:09 +0100

    Use Firebase Admin for post creation

--- actions/createPostAction.ts
+++ actions/createPostAction.ts
@@ -2,13 +2,17 @@
 
 import { currentUser } from '@clerk/nextjs/server';
 import { IUser } from '../types/user';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'; 
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { randomUUID } from 'crypto';
 import { revalidatePath } from 'next/cache';
 import { dbAdmin } from '@/firebase/admin';
 
-export async function createPostAction(data: { text: string, imageBase64?: string }) {
+export async function createPostAction(data: {
+  text: string;
+  imageBase64?: string;
+}) {
   const user = await currentUser();
+
   if (!user) {
     throw new Error('User not authenticated');
   }
@@ -36,34 +40,40 @@ export async function createPostAction(data: { text: string, imageBase64?: strin
       const secretAccessKey = process.env.CLOUDFLARE_R2_SECRET_KEY;
 
       if (!accessKeyId || !secretAccessKey) {
-        throw new Error("Cloudflare R2 credentials are not set in the environment variables.");
+        throw new Error(
+          'Cloudflare R2 credentials are not set in the environment variables.'
+        );
       }
 
       const s3 = new S3Client({
-        endpoint: 'https://4b46c9ea0c0a2600df0ac627dd90a047.r2.cloudflarestorage.com', 
+        endpoint:
+          'https://4b46c9ea0c0a2600df0ac627dd90a047.r2.cloudflarestorage.com',
         credentials: {
           accessKeyId,
           secretAccessKey,
         },
-        region: 'auto', 
+        region: 'auto',
       });
 
       const timestamp = Date.now();
       const file_name = `${randomUUID()}_${timestamp}.png`;
 
-      const base64Data = imageBase64.split(',')[1];  
+      const base64Data = imageBase64.split(',')[1];
+
       if (base64Data) {
         const buffer = Buffer.from(base64Data, 'base64');
-        const uploadParams = {
-          Bucket: 'networking-platform-images', 
+
+        const command = new PutObjectCommand({
+          Bucket: 'networking-platform-images',
           Key: file_name,
           Body: buffer,
           ContentType: 'image/png',
-        };
+        });
 
-        const command = new PutObjectCommand(uploadParams);
         await s3.send(command);
+
         image_url = `https://pub-8f5fd6db57a04cdaaa2c94011e2c3d5b.r2.dev/${file_name}`;
+
         console.log('Image uploaded successfully to Cloudflare R2:', image_url);
       }
     }
@@ -76,9 +86,9 @@ export async function createPostAction(data: { text: string, imageBase64?: strin
     };
 
     await dbAdmin.collection('posts').add(postData);
-
   } catch (error) {
-    console.log("Error creating post:", error);
+    console.log('Error creating post:', error);
+
     throw new Error(
       `Error creating post: ${
         error instanceof Error ? error.message : String(error)
